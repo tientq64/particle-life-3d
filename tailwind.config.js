aa22966dc74cc7c7d07b1b37c3d876693e3b2b65
tientq64 <tientq64@gmail.com>
2024-06-05 17:44:10 +0700

    Fix: Tailwindcss `content` config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './src/ui.tsx'],
+	content: ['./index.html', './src/**/*.{tsx,ts}'],
 	theme: {
 		extend: {}
 	},
